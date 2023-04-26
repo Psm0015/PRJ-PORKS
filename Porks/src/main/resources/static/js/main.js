ae3b1317ac1bd5dc431bf6ff908e74ed34a49a91
Psm0015 <pedro.mota@df.estudante.senai.br>
2023-04-26 17:22:18 -0300

    login feito

--- Porks/src/main/resources/static/js/main.js
+++ Porks/src/main/resources/static/js/main.js
@@ -1,15 +1,42 @@
 function dadosusr() {
     tk = sessionStorage.getItem("token");
-    $.ajax({
-        type: "GET",
-        url: "user/info",
-        headers: {
-            "Authorization": `Bearer ${tk}`
-        },
-        success: function(response) {
-            console.log(response);
-        }
-    });
+    if (tk != null) {
+        $.ajax({
+            type: "GET",
+            url: "user/info",
+            headers: {
+                "Authorization": `Bearer ${tk}`
+            },
+            success: function (response) {
+                sessionStorage.setItem("nome", response.nome)
+                sessionStorage.setItem("email", response.email)
+                sessionStorage.setItem("cpf", response.cpf)
+                sessionStorage.setItem("role", response.role)
+                let datansc = new Date(response.nascimento);
+                let dia = datansc.getDate()+1;
+                let mes = datansc.getMonth() + 1; // Adiciona 1 pois o mês começa em zero
+                let ano = datansc.getFullYear();
     
+                let dataFormatada = dia + "/" + mes + "/" + ano;
+                sessionStorage.setItem("nascimento", dataFormatada);
+                document.getElementById('tt').innerHTML = `Olá, ${response.nome}`
+                document.getElementById('emailmodal').innerHTML = `Email: ${response.email}`
+                document.getElementById('nascimentomodal').innerHTML = `Data de Nascimento: ${dataFormatada}`
+            }
+        });
+    }
 }
-dadosusr()
+function loginmodal() {
+    if (sessionStorage.getItem("token") != null) {
+        $('#lgmdl').modal('show');
+    }else{
+        window.location.href = 'login-1.html'
+    }
+}
+function encerrarSessao() {
+    sessionStorage.clear()
+    window.location.href = 'login-1.html'
+}
+if (sessionStorage.getItem("token") != null) {
+    dadosusr();
+}
